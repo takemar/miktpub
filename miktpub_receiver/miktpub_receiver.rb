0c4a3081bc28a015562e445bc0fa2d31a2a55fca
takemaro <info@takemaro.com>
2023-09-04 21:17:34 +0900

    Ctrl+CがRack/Pumaに取られてmikutterを終了できなくなる

--- miktpub_receiver/miktpub_receiver.rb
+++ miktpub_receiver/miktpub_receiver.rb
@@ -14,12 +14,35 @@ end
 
 Plugin.create(:miktpub_receiver) do
 
+  def sigint_default
+    Reserver.new(1) do
+      Delayer.new do
+        before = Signal.trap(:INT, 'DEFAULT')
+        if before == 'DEFAULT'
+          sigint_default
+        end
+      end
+    end
+  end
+
   Delayer.new do
     Thread.new do
-      Rackup::Server.start({
+      s = Rackup::Server.new({
         Port: 3939,
         app: Plugin::MiktpubReceiver::Application.new
       })
+      sigint_default
+      block_value = nil
+      s.start do |x|
+        block_value = x
+      end
+    ensure
+      case
+      when s.server.respond_to?(:shutdown) # WEBrick
+        s.server.shutdown
+      when block_value.respond_to?(:stop) # puma
+        block_value.stop
+      end
     end
   end
 end
