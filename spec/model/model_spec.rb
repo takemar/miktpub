5dab932a8b86311abc1930132e2921672a70784b
takemaro <info@takemaro.com>
2023-09-06 23:14:39 +0900

    add first test (red)

--- spec/model/model_spec.rb
+++ spec/model/model_spec.rb
@@ -1,6 +1,26 @@
+require 'json'
 require 'diva'
 require './miktpub_model/model'
+require './miktpub_model/as'
 
-RSpec.describe do
+RSpec.describe Plugin::MiktpubModel::Model do
 
+  let(:example_json) { File.read('./spec/fixtures/files/create_note.json') }
+  let(:example_hash) { JSON.parse(example_json, symbolize_names: true) }
+
+  it 'JSON-LD expanded形式のハッシュでnewできる' do
+    model = Plugin::MiktpubModel::Model.new(example_hash)
+    expect(model).to be_kind_of(Plugin::MiktpubModel::Model)
+    expect(model).to be_kind_of(Plugin::MiktpubModel::AS::Create)
+    expect(model).to be_kind_of(Plugin::MiktpubModel::AS::Activity)
+    expect(model).to be_kind_of(Plugin::MiktpubModel::AS::BaseObject)
+    expect(model.class).to eq(Plugin::MiktpubModel::Model[Plugin::MiktpubModel::AS::Create])
+    expect(model.published!).to eq(Time.new('2007-08-31T09:00:00Z'))
+    expect(model.object!).to be_kind_of(Plugin::MiktpubModel::Model)
+    expect(model.object!).to be_kind_of(Plugin::MiktpubModel::AS::Note)
+    expect(model.object!).to be_kind_of(Plugin::MiktpubModel::AS::BaseObject)
+    expect(model.object!.class).to eq(Plugin::MiktpubModel::Model[Plugin::MiktpubModel::AS::Note])
+    expect(model.object!.content!).to eq('Hello World!')
+    expect(model.object!.attributed_to!.name!).to eq('みくったーちゃん')
+  end
 end
--- spec/support/mikutter.rb
+++ spec/support/mikutter.rb
@@ -1,16 +1,21 @@
 class Plugin
 
+  def _(x); x; end
+
   class << self
     def [](...); new; end
     def call(...); end
-    def filtering(...); end
+    def filtering(event_names, *args); args end
   end
-
-  def _(x); x; end
 end
 
 module Diva; class Model
+
+  module MessageMixin; end
+  module UserMixin; end
+
   class << self
     def register(...); end
   end
+
 end; end
