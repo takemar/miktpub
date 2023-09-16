dc0c49b500cebdf12cde10e88117a93eb89d7f77
takemaro <info@takemaro.com>
2023-09-16 17:09:45 +0900

    tmp

--- miktpub/miktpub.rb
+++ miktpub/miktpub.rb
@@ -4,7 +4,7 @@ require_relative 'user'
 
 Plugin.create(:miktpub) do
 
-  defevent :miktpub_users, prototype: [String, Pluggaloid::COLLECT]
+  defevent :miktpub_query_local_users, prototype: [Hash, Pluggaloid::COLLECT]
 
   defdsl :miktpub_acct do |username, host|
     collect(:miktpub_users, host).filter do |user|
